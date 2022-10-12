9c78bac67d743ef3015ef25e7274881774aeea5d
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2022-10-12 14:04:02 +0800

    adding missing logic for google services

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:notification_background/local_data_source.dart';
 import 'dart:async';
 
 import 'package:notification_background/objectbox.g.dart';
@@ -9,9 +10,18 @@ final sl = GetIt.instance;
 
 Future<void> slInit() async {
   await registerSharedDep(sl);
+  sl.registerLazySingleton<LocalDB>(() => LocalDBImpl(store: sl.get<Store>()));
 }
 
-Future<Store?> get _getStore async {
+
+
+Future<void> registerSharedDep(GetIt sl) async {
+  final store = await _getStore;
+  sl.registerLazySingleton(() {
+    return store;
+  });
+}
+Future<Store> get _getStore async {
   final appdirectory = await getApplicationDocumentsDirectory();
   if (Store.isOpen(join(appdirectory.path, "objectBoxs"))) {
     return Store.attach(getObjectBoxModel(), join(appdirectory.path, "objectBoxs"));
@@ -20,11 +30,4 @@ Future<Store?> get _getStore async {
       directory: join(appdirectory.path, "objectBoxs"),
     );
   }
-}
-
-Future<void> registerSharedDep(GetIt sl) async {
-  final _store = await _getStore;
-  sl.registerLazySingleton(() {
-    return _getStore;
-  });
 }
